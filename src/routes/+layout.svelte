0fb2fff067831d09d7225d75e4fea0b0e99028d4
Ryu Martinez <ryueduardomartinez@outlook.es>
2024-11-19 08:33:36 -0300

    added auth logic

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,25 @@
 <script lang="ts">
 	import '../app.css';
-	let { children } = $props();
+    import '../global-styles/tailwind-base.css';
+    import { invalidate } from '$app/navigation'
+    import { onMount } from 'svelte'
+    import Navbar from '../components/layout/navbar.svelte';
+    import BottomNavbar from '../components/layout/bottom-navbar.svelte';
+
+    let { children } = $props();
+
+    export let data;
+    $: ({ session, supabase } = data);
+
+    onMount(() => {
+        const { data } = supabase.auth.onAuthStateChange((_, newSession) => {
+            if (newSession?.expires_at !== session?.expires_at) {
+                invalidate('supabase:auth');
+            }
+        });
+
+        return () => data.subscription.unsubscribe();
+    });
 </script>
 
 {@render children()}
