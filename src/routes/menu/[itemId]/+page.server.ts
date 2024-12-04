04ca92aa049b878136fbdd5e3799142360fa3a7a
Ryu Martinez <ryueduardomartinez@outlook.es>
2024-12-04 13:50:53 -0300

    create types library

--- src/routes/menu/[itemId]/+page.server.ts
+++ src/routes/menu/[itemId]/+page.server.ts
@@ -1,8 +1,9 @@
 import type { PageServerLoad } from './$types'
 import { error } from '@sveltejs/kit';
+import type {MenuItem} from "$lib/types/menu-item";
 
 export const load: PageServerLoad = async ({ locals: { supabase }, params }) => {
-	const  itemList  = await supabase.from('MenuItem').select('*')
+	const  itemList: MenuItem[]  = await supabase.from('MenuItem').select('*')
 	const item = itemList.find(x => x.id === params.itemId)
 
 	if (!item) {
